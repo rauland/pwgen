673f63d85fb9b90b16453bb4d5d760b43fdfc8c4
codenor <95989314+codenor@users.noreply.github.com>
2022-02-16 18:23:35 +1100

    Added Functions & fixed while loop

--- pwgen.py
+++ pwgen.py
@@ -8,36 +8,42 @@ while True:
         chars = string.ascii_letters + "!@#$%^&*()" + string.digits
         length = input("How many characters do you want your password to be?: ")
         count = input("How many passwords do you want to generate?: ")
+        csvinput = (input("Do you want to create a .csv file?: (Y / N) "))
         count_pass = int(count)
         count_pass_list = list(count)
         length_pass = int(length)
+        csv_answer = str(csvinput).lower()
+        Basefilename = "passwords"
+        Secondfilename = str(uuid.uuid4())
+        CSVfiletag = ".csv"
+        filename = f"{Basefilename}-{Secondfilename}{CSVfiletag}"
+    except Exception as e:
+        print("ERROR: (INVALID INPUT/S) PLEASE TRY AGAIN!")
+        continue
+    else: 
         break
-    except ValueError:
-        print("ERROR: (INVALID INPUT) USE INTERGERS ONLY! PLEASE TRY AGAIN!")
 
 def Passwordgen():
     password = "".join(secrets.choice(chars) for i in range(length_pass))
     return password
 
-password_list = list(Passwordgen()for i in range(count_pass))
-
-
-csv = input("Do you want to create a .csv file?: (Y / N) ")
-csv_answer = str(csv).lower()
-if csv_answer == "y" or csv_answer == "yes":
-    Basefilename = "passwords"
-    Secondfilename = str(uuid.uuid4())
-    CSVfiletag = ".csv"
-    filename = f"{Basefilename}-{Secondfilename}{CSVfiletag}"
+def csvcreate():
     with open(filename, "w") as f:
         for row in password_list:
             for x in row:
                 f.write(str(x))
-            f.write('\n')
-        print(f"{filename} has been created!")
-elif csv_answer == "n" or csv_answer == "no":
-    for i in range(count_pass):
-        print (Passwordgen())
-else:
-    print ("ERROR: (INVALID INPUT) YES/NO ONLY! PLEASE TRY AGAIN!")
+            f.write
+    print(f"{filename} has been created!")
     (exit)
+
+def csvorgen():
+    if csv_answer == "y" or csv_answer == "yes":
+        csvcreate()
+    elif csv_answer == "n" or csv_answer == "no":
+        for i in range(count_pass):
+            print (Passwordgen())
+            (exit)
+
+password_list = list(Passwordgen()for i in range(count_pass))
+
+csvorgen()
