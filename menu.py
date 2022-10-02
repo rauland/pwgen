024818398f9e1f1e6311c27616267bd934fa2be9
codenor <95989314+codenor@users.noreply.github.com>
2022-10-03 01:10:14 +1100

    feat: password generator menu added to menu.py

--- menu.py
+++ menu.py
@@ -1,8 +1,14 @@
 from consolemenu import *
 from consolemenu.items import *
+from csvgen import csvgen
 
-def main():
+def mainmenu():
+    """Function for main menu"""
     menu = ConsoleMenu("PasswordGen", "A simple password manager made in python.")
+    function_item = FunctionItem("Password Generator", csvgen) # Menu for password generator
+
+    # Adding item menu
+    menu.append_item(function_item) 
     menu.show()
 
-main()
+mainmenu()
