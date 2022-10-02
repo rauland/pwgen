39090f29a909741674b2bb42503c712c5ed726ed
codenor <95989314+codenor@users.noreply.github.com>
2022-10-02 20:18:34 +1100

    feat: simple menu

--- menu.py
+++ menu.py
@@ -1,2 +1,8 @@
 from consolemenu import *
 from consolemenu.items import *
+
+def main():
+    menu = ConsoleMenu("PasswordGen", "A simple password manager made in python.")
+    menu.show()
+
+main()
