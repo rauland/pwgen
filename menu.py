cc580287660ec339052f6f326400e218b5308518
codenor <95989314+codenor@users.noreply.github.com>
2022-10-05 01:53:22 +1100

    feat: added rest of menu options in console menu

--- menu.py
+++ menu.py
@@ -1,7 +1,7 @@
 from consolemenu import *
 from consolemenu.items import *
 from csvgen import csvgen
-from account import Account, Master, load
+from account import *
 
 def mainmenu():
     """Function for main menu"""
@@ -13,12 +13,18 @@ def mainmenu():
         account = Master("admin@url.com.au","password")
 
     menu = ConsoleMenu("PasswordGen", "A simple password manager made in python.")
-    function_item = FunctionItem("Password Generator", csvgen) # Menu for password generator
-    function_item_1 = FunctionItem("Add Password", account.addpw)
+    function_item = FunctionItem("Password generator", csvgen) # Menu for password generator
+    function_item_1 = FunctionItem("Add password", account.addpw)
+    function_item_2 = FunctionItem("Show list of passwords", account.showpw)
+    function_item_3 = FunctionItem("Save account", account.save)
+    function_item_4 = FunctionItem("Export account", account.export_acc)
 
     # Adding item menu
     menu.append_item(function_item)
-    menu.append_item(function_item_1) 
+    menu.append_item(function_item_1)
+    menu.append_item(function_item_2)
+    menu.append_item(function_item_3)
+    menu.append_item(function_item_4)
     menu.show()
 
 mainmenu()
