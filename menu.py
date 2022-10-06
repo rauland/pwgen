a3ff3c4be550cb3d53cdb879a798c6d578753af9
codenor <95989314+codenor@users.noreply.github.com>
2022-10-07 08:41:27 +1100

    feat: add tip to menu

--- menu.py
+++ menu.py
@@ -21,7 +21,7 @@ def mainmenu():
     Path("saved-accounts").mkdir(parents=True, exist_ok=True)
     Path("csv-export").mkdir(parents=True, exist_ok=True)
 
-    menu = ConsoleMenu(print_logo, show_exit_option=False)
+    menu = ConsoleMenu(print_logo, epilogue_text="Tip: Type number to select menu item" ,show_exit_option=False)
     function_item = FunctionItem("Password generator", csvgen.generate) # Menu item for password generator
     function_item_1 = FunctionItem("Add password", account.addpw) #Menu item for adding password
     function_item_2 = FunctionItem("Show list of passwords", account.showpw) #Menu item for showing list of passwords
