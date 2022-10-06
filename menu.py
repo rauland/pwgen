ece1b644f9abd38383e7db22eae05f09c941f1ac
codenor <95989314+codenor@users.noreply.github.com>
2022-10-07 08:09:53 +1100

    fix: fix exit, for reals this time

--- menu.py
+++ menu.py
@@ -3,6 +3,10 @@ from consolemenu.items import *
 import csvgen
 from account import *
 from pathlib import Path
+from logo import print_logo
+
+def exitmenu():
+    exit()
 
 def mainmenu():
     """Function for main menu"""
@@ -17,12 +21,13 @@ def mainmenu():
     Path("saved-accounts").mkdir(parents=True, exist_ok=True)
     Path("csv-export").mkdir(parents=True, exist_ok=True)
 
-    menu = ConsoleMenu("PWGEN", "Simple terminal password manager made in python")
+    menu = ConsoleMenu(print_logo, show_exit_option=False)
     function_item = FunctionItem("Password generator", csvgen.generate) # Menu item for password generator
     function_item_1 = FunctionItem("Add password", account.addpw) #Menu item for adding password
     function_item_2 = FunctionItem("Show list of passwords", account.showpw) #Menu item for showing list of passwords
     function_item_3 = FunctionItem("Save account", account.save) #Menu item for saving account
     function_item_4 = FunctionItem("Export account", account.export_acc) #menu item for exporting account
+    function_item_5 = FunctionItem("Exit", exitmenu) #exit program function
 
     # Adding item menu
     menu.append_item(function_item)
@@ -30,6 +35,7 @@ def mainmenu():
     menu.append_item(function_item_2)
     menu.append_item(function_item_3)
     menu.append_item(function_item_4)
+    menu.append_item(function_item_5)
     menu.show()
 
 mainmenu()
