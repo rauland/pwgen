2cf2aec7b91bfb3aaeead7021ba7bc53cd6f356e
rauland <30706122+rauland@users.noreply.github.com>
2024-03-23 18:55:02 +1100

    Creating GUI generator

--- pwgenerator/gui.py
+++ pwgenerator/gui.py
@@ -1,6 +1,7 @@
 """GUI Module for PWGEN"""
 import ttkbootstrap as ttk
-from ttkbootstrap.constants import LEFT
+# from ttkbootstrap.constants import BOTH, YES
+from export import generator
 
 class App():
     """APP GUI Class"""
@@ -8,16 +9,51 @@ class App():
         self.root = ttk.Window(themename="cosmo")
 
         self.root.title("PWGEN")
-        self.b1 = ttk.Button(self.root, text="Submit", bootstyle="success")
-        self.b1.pack(side=LEFT, padx=5, pady=10)
+        self.root.geometry("600x480")
 
-        self.b2 = ttk.Button(self.root, text="Submit", bootstyle="info-outline")
-        self.b2.pack(side=LEFT, padx=5, pady=10)
+        # register the validation callback
+        digit_func = self.root.register(validate_number)
+        # alpha_func = self.root.register(validate_alpha)
 
+        # Components
+        self.l1 = ttk.Label(self.root, text="Password Generator", bootstyle="primary")
+        self.l1.pack(pady=10)
+
+        self.l2 = ttk.Label(self.root, text="Enter the password length", bootstyle="info")
+        self.l2.pack(pady=10)
+        self.e1 = ttk.Spinbox(self.root, bootstyle="info", validate="focus", validatecommand=(digit_func, '%P'), from_=0, to=100)
+        self.e1.pack(pady=10)
+        self.l3 = ttk.Label(self.root, text="How many passwords?", bootstyle="info")
+        self.l3.pack(pady=10)
+        self.c1 = ttk.Spinbox(self.root, bootstyle="primary", validate="focus", validatecommand=(digit_func, '%P'), from_=0, to=100)
+        self.c1.pack(pady=10)
+        self.b1 = ttk.Button(self.root, text="Submit", bootstyle="primary", command=generator())
+        self.b1.pack(padx=5, pady=10)
+
+        self.l4 = ttk.Label(self.root, text="Output goes here...", bootstyle="info")
+        self.l4.pack(pady=10)
 
     def run(self):
         """Run the GUI"""
         self.root.mainloop()
+
+def validate_number(x) -> bool:
+    """Validates that the input is a number"""
+    if x.isdigit():
+        return True
+    elif x == "":
+        return True
+    else:
+        return False
+
+def validate_alpha(x) -> bool:
+    """Validates that the input is alpha"""
+    if x.isdigit():
+        return False
+    elif x == "":
+        return True
+    else:
+        return True
 
 if __name__ == "__main__":
     app = App()
