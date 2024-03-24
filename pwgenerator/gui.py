5019634c0619db9fa13b5e2fcc81b75b5e5ee27a
rauland <30706122+rauland@users.noreply.github.com>
2024-03-24 23:12:53 +1100

    added form validation

--- pwgenerator/gui.py
+++ pwgenerator/gui.py
@@ -6,13 +6,13 @@ from pwgen import generate
 class App():
     """APP GUI Class"""
     def __init__(self):
-        self.root = ttk.Window(themename="cosmo")
+        self.root = ttk.Window(themename="morph")
 
         self.root.title("PWGEN")
         self.root.geometry("600x480")
 
         # Components
-        self.form = PwgenForm(self.root)
+        self.form = PwgenForm(self.root, padding=10)
         self.form.pack()
 
     def run(self):
@@ -34,40 +34,50 @@ class PwgenForm(ttk.Frame):
         self.l1 = ttk.Label(self, text="Password Generator", bootstyle="primary", font=("Helvetica", 16))
         self.l1.pack(pady=10)
 
-        self.l2 = ttk.Label(self, text="Enter the password length", bootstyle="info", font=("Helvetica", 10))
+        self.l2 = ttk.Label(self, text="Enter the password length", bootstyle="primary", font=("Helvetica", 10))
         self.l2.pack(pady=10)
-        self.e1 = ttk.Spinbox(
-            self,
-            bootstyle="info",
-            validate="focus",
-            validatecommand=(digit_func, '%P'),
-            from_=0, to=100)
-
-        self.e1.pack(pady=10)
-        self.l3 = ttk.Label(self, text="How many passwords?", bootstyle="info", font=("Helvetica", 10))
-        self.l3.pack(pady=10)
-        self.c1 = ttk.Spinbox(
-            self,
+        self.e1 = ttk.Spinbox(self,
             bootstyle="primary",
             validate="focus",
             validatecommand=(digit_func, '%P'),
-            from_=0, to=100)
+            from_=0,
+            to=100)
+
+        self.e1.pack(pady=10)
+        self.l3 = ttk.Label(self,
+            text="How many passwords?",
+            bootstyle="primary",
+            font=("Helvetica", 10))
+
+        self.l3.pack(pady=10)
+        self.c1 = ttk.Spinbox(self,
+            bootstyle="primary",
+            validate="focus",
+            validatecommand=(digit_func, '%P'),
+            from_=0,
+            to=100)
 
         self.c1.pack(pady=10)
-        self.b1 = ttk.Button(
-            self, text="Submit", bootstyle="primary",
+        self.b1 = ttk.Button(self,
+            text="Submit",
+            bootstyle="primary",
             command=lambda: generated_output(self.e1.get(),self.c1.get(),self.l4))
         self.b1.pack(padx=5, pady=10)
 
-        self.l4 = ttk.Label(self, text="Output goes here...", bootstyle="info")
-        self.l4.pack(pady=10)
+        self.l4 = ttk.Text(self)
+        # self.l4 = ttk.Text(self, text="Output goes here...", bootstyle="info")
+        self.l4.insert(1.0, "Output goes here...")
+        self.l4.configure(state="disabled")
+        self.l4.pack()
 
 class Validate():
     """GUI Field Validation"""
     def number(self, x) -> bool:
         """Validates that the input is a number"""
         if x.isdigit():
-            return True
+            if int(x) <= 100:
+                return True
+            else: return False
         elif x == "":
             return True
         else:
@@ -81,11 +91,17 @@ class Validate():
         else:
             return True
 
-def generated_output(length,count,output):
+def generated_output(length, count, text):
     """Sets output for GUI"""
-    output_list = generate.many_secrets(int(length),int(count),False,True)
-    label_text = '\n'.join(output_list)
-    output.config(text=label_text)
+    if validate.number(length) and validate.number(count):
+        output = generate.many_secrets(int(length),int(count),False,True)
+        output = '\n'.join(output)
+    else:
+        output = "Error: No Characters or Length or Amount over 100"
+    text.configure(state="normal")
+    text.delete(1.0, "end")
+    text.insert(1.0, output)
+    text.configure(state="disabled")
 
 validate = Validate()
 
